9ad00839c9d38a9fad917473a7f82d632a1986bf
bwrsandman <mr.sandy.carter@gmail.com>
2013-10-07 20:33:41 -0400

    Add gzip, h3m file support no matter the extension

--- readmap.lua
+++ readmap.lua
@@ -5,10 +5,16 @@ require 'h3map'
 local mapdir = "TestMaps"
 
 function love.load()
+    gunzipper = zlib.inflate()
     for i, mapname in ipairs(love.filesystem.enumerate(mapdir)) do
         local filename = mapdir.."/"..mapname
-        if love.filesystem.isFile(filename) and not mapname:find('[.]gz$') then
+        if love.filesystem.isFile(filename) then
             local contents, size = love.filesystem.read(filename)
+            -- Check for gzip magic number
+            if contents:sub(0, 2) == "\x1f\x8b" then
+                local inflated, eof, bytes_in, bytes_out = gunzipper(contents)
+                contents = inflated
+            end
             print("Found map: "..filename)
             print("==========="..string.rep("=",filename:len()))
             print()
